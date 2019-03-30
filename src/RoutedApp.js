70de7bc1b9c9f413fd36a65b18ea520eef31f553
Jerry Lung <jzlung@gmail.com>
2019-03-30 12:09:41 -0700

    multi cards and proper routing

--- src/RoutedApp.js
+++ src/RoutedApp.js
@@ -2,12 +2,14 @@ import React from "react";
 import './App.scss';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SectionView from './components/SectionView';
+import SectionCard from './components/SectionCard';
 
 import metadata from './metadata.json';
 
 const sectionsData = metadata.programs["1"].sections;
 const section1Data = metadata.programs["1"].sections[0];
 
+// TODO: move to helper file, and also sanitize special characters
 const getUrlFromSectionName = name => {
   return name.toLowerCase().replace(' ', '-');
 }
@@ -18,7 +20,7 @@ function RoutedApp() {
     <Router>
       <div className="modern-health-app">
 
-        <Route exact path="/" component={MainMenu} />
+        <Route exact path="/" render={props => <Program {...props} data={metadata.programs["1"]} />} />
         <Route 
           exact path="/:section" 
           render={props => <SectionView {...props} data={sectionsData} currentIndex={1} onReturn={() => {}} />} 
@@ -37,6 +39,29 @@ function MainMenu() {
     <Link to={`/${getUrlFromSectionName(section1Data.name)}`} >
       {section1Data.name}
     </Link>
+  );
+}
+
+function Program(props) {
+  return (
+    <div className="program-preview">
+      <h1>{props.data.name}</h1>
+
+      <a href="#">Learn More</a>
+
+      <div className="sections">
+
+        {props.data.sections.map((section, sectionIndex) => {
+          return (
+            <Link to={`/${getUrlFromSectionName(section1Data.name)}/${sectionIndex}`} >
+              <SectionCard data={section} />
+            </Link>
+          );
+          
+        })}
+
+      </div>
+    </div>
   );
 }
 
