571be0d082a385d09ad0cb513c6c7a0da6f56a26
Jerry Lung <jzlung@gmail.com>
2019-04-01 00:55:17 -0700

    local storage functionality added

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,14 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers/index";
+import { loadState, saveState } from "../localStorage";
 
-const store = createStore(rootReducer);
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState);
+
+store.subscribe(() => {
+  saveState({
+    userActivity: store.getState().userActivity,
+  });
+});
 
 export default store;
