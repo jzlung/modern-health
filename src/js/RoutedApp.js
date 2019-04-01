313a4319739db295cc3703cea0b374151937c9ec
Jerry Lung <jzlung@gmail.com>
2019-03-31 22:12:40 -0700

    all programs in

--- src/js/RoutedApp.js
+++ src/js/RoutedApp.js
@@ -7,19 +7,22 @@ import metadata from '../metadata.json';
 
 // TODO: move to helper file, and also sanitize special characters
 const getHashFromSectionName = name => {
-  return name.toLowerCase().replace(' ', '-');
+  return name.toLowerCase().split('').filter(char => /[a-z ]/.test(char)).join('').replace(/ /g, '-');
 };
 
-const getSectionDataFromHash = hash => {
+const getSectionDataFromHash = (program, hash) => {
   // Todo Hardcoded
-  return metadata.programs["1"].sections.find(sectionData => getHashFromSectionName(sectionData.name) === hash);
+  return metadata.programs[program].sections.find(sectionData => getHashFromSectionName(sectionData.name) === hash);
 };
 
 const renderSection = (props) => {
-  // localhost:3000/1/mindfulness/0
-
+  const params = props.match.params;
   return (
-    <SectionView {...props} data={getSectionDataFromHash(props.match.params.section)} programId={props.match.params.programId} />
+    <SectionView 
+      {...props} 
+      data={getSectionDataFromHash(params.programId, props.match.params.section)} 
+      programId={params.programId} 
+    />
   );
 };
 
@@ -47,7 +50,7 @@ function MainMenu() {
   return (
     <div>
       {Object.keys(metadata.programs).map(programId => (
-        <Program data={metadata.programs[programId]} key={programId} />
+        <Program data={metadata.programs[programId]} key={programId} programId={programId} />
       ))}
     </div>
   );
@@ -64,10 +67,11 @@ function Program(props) {
 
         {props.data.sections.map(section => {
           const hashedSectionName = getHashFromSectionName(section.name);
+          const link = `/${props.programId}/${hashedSectionName}`;
           return (
             <Link 
-              to={`/1/${hashedSectionName}/0`} 
-              key={`/1/${hashedSectionName}`}
+              to={`${link}/0`} 
+              key={link}
             >
               <SectionCard data={section} />
             </Link>
